1c80a486e43d5e1632e784e22b497629bbfaa12a
FloraQinQvQ <weimeng.qin18@gmail.com>
2026-03-09 22:40:18 +0000

    check with render

--- server/src/app.ts
+++ server/src/app.ts
@@ -29,7 +29,7 @@ app.get('/health', (_req, res) => res.json({ ok: true }));
 
 // Serve React app in production
 if (isProd) {
-  const clientDist = path.join(__dirname, '../../client/dist');
+  const clientDist = path.join(__dirname, '../../../../client/dist');
   app.use(express.static(clientDist));
   app.get('*', (_req, res) => res.sendFile(path.join(clientDist, 'index.html')));
 }
